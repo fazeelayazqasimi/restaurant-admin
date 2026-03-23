6e6b527b1f74a9f625a25fc99fd806bc1c4a71cd
fazeelayazqasimi <fazeelayazqasimi31@gmail.com>
2026-03-24 04:49:51 +0500

    again API updated

--- services/api.js
+++ services/api.js
@@ -1,21 +1,24 @@
 import axios from 'axios'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 const API_URL = 'https://restaurant-server-snowy.vercel.app/api' // Live
 
-
 const api = axios.create({
   baseURL: API_URL,
-  headers: { 'Content-Type': 'application/json' }
+  headers: {
+    'Content-Type': 'application/json'
+  }
 })
 
-api.interceptors.request.use((config) => {
-  if (typeof window !== 'undefined') {
-    const token = localStorage.getItem('admin_token')
+api.interceptors.request.use(
+  async (config) => {
+    const token = await AsyncStorage.getItem('token')
     if (token) {
       config.headers.Authorization = `Bearer ${token}`
     }
-  }
-  return config
-})
+    return config
+  },
+  (error) => Promise.reject(error)
+)
 
 export default api
