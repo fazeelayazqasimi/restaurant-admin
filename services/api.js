d23e38c85293fd88e342253e25b8d57418c5f4ec
Fazeel Ayaz Qasimi <154905970+fazeelayazqasimi@users.noreply.github.com>
2026-04-07 04:14:45 +0500

    Update api.js

--- services/api.js
+++ services/api.js
@@ -1,8 +1,6 @@
 import axios from 'axios'
 
-const API_URL = process.env.NODE_ENV === 'development'
-  ? 'http://localhost:5000/api'                          // Local
-  : 'https://restaurant-server-snowy.vercel.app/api'  // Live
+const API_URL = 'https://restaurant-server-snowy.vercel.app/api'
 
 const api = axios.create({
   baseURL: API_URL,
@@ -12,11 +10,21 @@ const api = axios.create({
 api.interceptors.request.use((config) => {
   if (typeof window !== 'undefined') {
     const token = localStorage.getItem('admin_token')
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`
-    }
+    if (token) config.headers.Authorization = `Bearer ${token}`
   }
   return config
 })
+
+api.interceptors.response.use(
+  res => res,
+  err => {
+    if (err.response?.status === 401 && typeof window !== 'undefined') {
+      localStorage.removeItem('admin_token')
+      localStorage.removeItem('admin_user')
+      window.location.href = '/login'
+    }
+    return Promise.reject(err)
+  }
+)
 
 export default api
